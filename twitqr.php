19964853a088bf3253799b10955a6d081f29ef14
pabloguti <guti.bierzo@gmail.com>
2013-01-04 13:42:17 +0100

    Update twitqr.php
    Mejoras del código original de @minipunk
    
    - No hace falta FTP para subir la imagen, se hace con un copy()
    - Se buscan posibles errores
    - Responde a múltiples menciones seguidas

--- twitqr.php
+++ twitqr.php
@@ -1,5 +1,5 @@
 <?php
-$file = "respondido.txt"; // Para no responder dos veces al mismo, almacena el ID del tweet
+$file = "respondido.txt"; // Almacena el ID del útlimo tweet respondido
 
 // compruebo que existe el archivo txt y si no existe lo crea
 if(!file_exists($file)){
@@ -24,74 +24,78 @@ $tmhOAuth = new tmhOAuth(array(
   'user_secret'     => 'SECRET DE USUARIO',
 ));
 
-$code = $tmhOAuth->request('GET', $tmhOAuth->url('1.1/statuses/mentions_timeline.json')); // recupero menciones de twitter
+//En caso de que since_id ya exista (>0) le pido a twitter las menciones desde el último
+if ($since_id != 0) {
+	$params = array('since_id' => $since_id);
 
-$mentions = json_decode($tmhOAuth->response['response']); // Decodifico el archivo JSON
+	$code = $tmhOAuth -> request('GET', $tmhOAuth -> url('1.1/statuses/mentions_timeline.json'), $params);
+	// recupero menciones de twitter
+} else {
+	$code = $tmhOAuth -> request('GET', $tmhOAuth -> url('1.1/statuses/mentions_timeline.json'));
+}
+$mentions = json_decode($tmhOAuth -> response['response']);
+// Decodifico el archivo JSON
 
-  $tweetqr = $mentions[0]->text; //utilizo solamente la última mención
-  $since_id = $mentions[0]->id; // id del último tweet
+$last_id = 0;
+$last_user = '';
+if (!isset($mentions -> errors)) {//Si twitter NO devuelve un error, proceso
+	foreach ($mentions as $mention)//Recorro todas las menciones
+	{
+		$tweetqr = procesa_text($mention -> text);
+		//Proceso el texto
+		$filename = get_image_qr($tweetqr, $mention -> id);
+		//Pido la imagen QR
+		$handle = fopen($filename, "rb");
+		$image = fread($handle, filesize($filename)); //Obtengo la info de la imagen
+		fclose($handle);
+		$user = $mention -> user -> screen_name;
+		$status = "@$user aquí tienes tu QR ";
+		$filename = $mention -> id . ".jpg"; //Nombre del archivo de la imagen
+		$code = $tmhOAuth -> request('POST', 'https://api.twitter.com/1.1/statuses/update_with_media.json', 
+			array('media[]' => "{$image};type=image/jpeg;filename={$filename}", 
+			"status" => ' ' . $status), 
+			true, // uso auth
+			true // multipart
+		);
+		if ($mention -> id > $last_id) {//Compruebo si el id actual es el mayor para almacenarlo
+			$last_id = $mention -> id;
+		}
+		unlink($filename); //Elimino la foto del QR
+		if ($last_user == $user) {
+			/*ESPECIAL: Si tengo que responder 2 tweets seguidos a un usuario,
+			debo esperar para que twitter no de el fallo 403 */
+			sleep(10); //Espero 10 segundos
+		} else {
+			$last_user = $user;
+		}
+	}
 
-   if(($mentions->id)>$since_id){
-            $since_id = $mentions->id; // compruebo que la mención es nueva
-       
-      $handle = fopen($file, 'w+'); // abro y leo el archivo de texto con el ID del tweet
-      fwrite($handle, $since_id); // Escribo el nuevo id
-      fclose($handle); // cierro el archivo de texto
-    
-	  
-	$cambia  = array(' ');
-	$replace = array('+');
-	$tweetqr = str_replace($cambia, $replace, $tweetqr); // reemplazo espacios por signo +
- 
-$paths = '/'; // ruta del hosting donde almacenar la foto
-$filep = "https://chart.googleapis.com/chart?chs=545x545&cht=qr&chl=$tweeqr"; // genero la imagen con la api de google
- 
-$ftp_server = 'ftp.dominioa.es'; // dirección ftp al servidor
- 
-$ftp_user_name = 'user'; // usuario del FTP
- 
-$ftp_user_pass = 'pass'; // contraseña del FTP
- 
-$name = 'foto.jpg'; // Nombre con que se guardará la imagen
- 
-$conn_id = ftp_connect($ftp_server); // CONECTO AL SERVIDOR
- 
-$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);// ENVIO EL LOGIN
- 
-// CHEKEO LOS RESULTADOS
-if ((!$conn_id) || (!$login_result)) {
-       echo "Error en la conexión FTP!";
-       echo "No se ha podido conectar a $ftp_server con el usuario $ftp_user_name....";
-       exit;
-   } else {
-       echo "Conectado a $ftp_server, con el usuario $ftp_user_name"."..... Se ha guardado la imagen generada";
-   }
- 
-$upload = ftp_put($conn_id, $paths.'public_html/'.$name, $filep, FTP_BINARY);// SUBO EL ARCHIVO DE IMAGEN
- 
-// CHEKEO LA SUBIDA
-if (!$upload) {
-       echo "Error al subir la imagen por FTP!";
-   } else {
-       echo "Guardada la imagen $name en $ftp_server ";
-   }
- 
-ftp_close($conn_id);	// CIERRO LA CONEXIÓN	
+	$handle = fopen($file, 'w+');
+	// abro y leo el archivo de texto con el ID del último tweet procesado
+	fwrite($handle, $last_id);
+	// Escribo el nuevo id
+	fclose($handle);
+	// cierro el archivo de texto
+} else {//En caso de error, muesto el error de Twitter
+	echo "Error en Twitter: " . $mentions -> errors[0] -> message;
 
-$filename = "foto.jpg"; // ruta de la imagen que publicamos en twitter (tiene que estar en el mismo directorio que este archivo).
-$handle = fopen($filename, "rb");
-$image = fread($handle, filesize($filename));
-fclose($handle);
-$status = "@$user aquí tienes tu QR "; //FRASE A TWITTEAR
- 
- // Publico el tweet con la imagen
-$code = $tmhOAuth->request('POST', 'https://api.twitter.com/1.1/statuses/update_with_media.json',
-  array(
-    'media[]' => "{$image};type=image/jpeg;filename={$filename}" ,
-   "status"   => ' '.$status 
-  ),
-  true, // uso auth
-  true  // multipart
-);
+}
+function get_image_qr($data, $id) { //Obtengo la imagen de Google
+	set_time_limit(120);
+	$source = "https://chart.googleapis.com/chart?chs=545x545&cht=qr&chld=H&chl=" . urlencode($data); 
+	//importante codificar la url 
+	$dest = $id . ".jpg"; //Selecciono el destino (id + .jpg)
+	$fd = fopen($source, "r") or die("<center>Unable to access source page</center>");
+	$pagina = fread($fd, sizeof($source));
+	copy($source, $dest); //Copy la imagen de google chart api la imagen a mi servidor
+	fclose($fd);
+	return $dest; //Devuelvo el nombre de la imagen
+}
+
+function procesa_text($text) {
+	$cambia = array('@creaqr'); //Elimino el nombre de la mención
+	$replace = array('');
+	$text = str_replace($cambia, $replace, $text);
+	return trim($text); //Elimino espacios al principio y fin
 }
 ?>
