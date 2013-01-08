3a8b925d5b282c1bfbb1d025c9c21f3550b245c5
pabloguti <guti.bierzo@gmail.com>
2013-01-08 14:06:37 +0100

    Update twitqr.php

--- twitqr.php
+++ twitqr.php
@@ -54,30 +54,27 @@ if (!isset($mentions -> errors)) {//Si twitter NO devuelve un error, proceso
 	{
 		$tweetqr = procesa_text($mention -> text);
 		//Proceso el texto
-		$filename = get_image_qr($tweetqr, $mention -> id);
-		//Pido la imagen QR
-		$handle = fopen($filename, "rb");
-		$image = fread($handle, filesize($filename)); //Obtengo la info de la imagen
-		fclose($handle);
-		$user = $mention -> user -> screen_name;
-		$status = "@$user aquí tienes tu QR ";
-		$filename = $mention -> id . ".jpg"; //Nombre del archivo de la imagen
-		$code = $tmhOAuth -> request('POST', 'https://api.twitter.com/1.1/statuses/update_with_media.json', 
-			array('media[]' => "{$image};type=image/jpeg;filename={$filename}", 
-			"status" => ' ' . $status), 
-			true, // uso auth
+		
+		if ($tweetqr != null) {
+			$filename = get_image_qr($tweetqr, $mention -> id);
+			$handle = fopen($filename, "rb");
+			$image = fread($handle, filesize($filename));
+			fclose($handle);
+			$user = $mention -> user -> screen_name;
+			$status = "@$user aquí tienes tu QR ";
+			$filename = $mention -> id . ".jpg";
+			$code = $tmhOAuth -> request('POST', 'https://api.twitter.com/1.1/statuses/update_with_media.json', array('media[]' => "{$image};type=image/jpeg;filename={$filename}", "status" => ' ' . $status), true, // uso auth
 			true // multipart
-		);
-		if ($mention -> id > $last_id) {//Compruebo si el id actual es el mayor para almacenarlo
-			$last_id = $mention -> id;
-		}
-		unlink($filename); //Elimino la foto del QR
-		if ($last_user == $user) {
-			/*ESPECIAL: Si tengo que responder 2 tweets seguidos a un usuario,
-			debo esperar para que twitter no de el fallo 403 */
-			sleep(10); //Espero 10 segundos
-		} else {
-			$last_user = $user;
+			);
+			if ($mention -> id > $last_id) {//Obtengo el id mayor para almacenarlo
+				$last_id = $mention -> id;
+			}
+			unlink($filename);
+			if ($last_user == $user) {
+				sleep(10);
+			} else {
+				$last_user = $user;
+			}
 		}
 	}
 
@@ -104,9 +101,15 @@ function get_image_qr($data, $id) { //Obtengo la imagen de Google
 }
 
 function procesa_text($text) {
-	$cambia = array('@creaqr'); //Elimino el nombre de la mención
-	$replace = array('');
-	$text = str_replace($cambia, $replace, $text);
-	return trim($text); //Elimino espacios al principio y fin
+	$array_text = explode(' ', $text);
+	if ($array_text[0] == '@creaqr') {
+		$cambia = array('@creaqr'); //Elimino el nombre de la mención
+		$replace = array('');
+		$text = str_replace($cambia, $replace, $text);
+		$text = trim($text); //Elimino espacios al principio y fin
+	} else {
+		$text = null;
+	}
+	return $text;
 }
 ?>
